813790f5d66dc7dce685dbf281e68e4ddb337624
sb-2016 <swarnendu.banerjee2017@gmail.com>
2024-03-11 20:15:21 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+# print("Hello, World!")
+# print(2+3)
+# import pandas
+# import numpy
+# import matplotlib.pyplot as plt
+# import seaborn
+# print("Hello World" , 5)
+# print(2+3)
+# print("Chota charbhighay gass khyay")
+# print(17*99)
+print("Hello, World!\nChota is harami")
+
+# this is a comment
+print("Hello, World!")  # this is also a comment but after a line of code
